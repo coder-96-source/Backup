95fc215418185c67c6cbfce1e07be7fcc6dcb0a5
Jason Kim <Jason Kim@DESKTOP-7IN0T8L>
2017-09-10 21:21:55 +0900

    BubbleSort

---
+++ CSharp/1_Web_Application/PG4_Web_MyArticles/Lab43/MyArticles/Models/TopicVM.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MyArticles.Models
+{
+    public class TopicVM
+    {
+        public IEnumerable<Topic> Topics { get; set; }
+        public PagingInfo PagingInfo { get; set; }
+    }
+}
---
+++ CSharp/3_Algorithm/PG3_BubbleSort/BubbleSort/PG3_BubbleSort/BubbleSort/BubbleSorter.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PG3_BubbleSort.BubbleSort
+{
+    public static class BubbleSorter
+    {
+        public static void BubbleSort<T>(List<T> elements) where T : IComparable<T>
+        {
+            int endIndex = elements.Count - 1;
+            bool flag = true;
+            
+            for (int i = 0; i < elements.Count - 1; i++) // do as much as n-1
+            {
+                while (flag) // to avoid unnecessary loop
+                {
+                    flag = false; // set flag as false to escape loop
+
+                    for (int j = 0; j < endIndex; j++)
+                    {
+                        if (elements[j].CompareTo(elements[j + 1]) > 0)
+                        {
+                            // swap
+                            var temp = elements[j];
+                            elements[j] = elements[j + 1];
+                            elements[j + 1] = temp;
+                            flag = true; // set flag as true to keep looping
+                        }
+                    }
+                    endIndex--;
+
+                }
+            }
+        }
+    }
+}
