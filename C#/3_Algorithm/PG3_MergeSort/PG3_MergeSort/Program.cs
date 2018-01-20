dc1f65a9ca3649d203ef973c43378bb7a2752dfb
Jason Kim <kims07231992@gmail.com>
2018-01-20 03:08:50 -0800

    Added PG3_MergeSort, reffered HackerRank Youtube

---
+++ C#/3_Algorithm/PG3_MergeSort/PG3_MergeSort/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PG3_MergeSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // test
+            int[] array = new int[] { 5, 4, 3, 2, 1, 10, 9, 8, 7, 6 };
+
+            Console.WriteLine("Unsorted");
+            foreach (int v in array)
+            {
+                Console.WriteLine(v);
+            }
+
+            MergeSort.MergeSorter.MergeSort(array);
+            Console.WriteLine("Sorted");
+            foreach (int v in array)
+            {
+                Console.WriteLine(v);
+            }
+        }
+    }
+}
