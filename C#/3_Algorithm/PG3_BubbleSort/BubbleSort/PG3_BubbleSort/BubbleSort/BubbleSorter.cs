cea6ad267fe22fc6552c9bf1ef2fd0d6e74b88b4
Jason Kim <kims07231992@gmail.com>
2018-01-28 22:32:34 -0800

    Amended wrong code of Bubblesort

--- C#/3_Algorithm/PG3_BubbleSort/BubbleSort/PG3_BubbleSort/BubbleSort/BubbleSorter.cs
+++ C#/3_Algorithm/PG3_BubbleSort/BubbleSort/PG3_BubbleSort/BubbleSort/BubbleSorter.cs
@@ -11,70 +11,68 @@ namespace PG3_BubbleSort.BubbleSort
         public static void BubbleSort<T>(List<T> elements) where T : IComparable<T>
         {
             int endIndex = elements.Count - 1;
-            bool flag = true;
+            bool swapped = true;
             
             for (int i = 0; i < elements.Count - 1; i++) // do as much as n-1
             {
-                while (flag) // to avoid unnecessary loop
-                {
-                    flag = false; // set flag as false to escape loop
+                if (!swapped) // to avoid unnecessary loop
+                    break;
 
-                    for (int j = 0; j < endIndex; j++)
+                swapped = false; // set flag as false to escape loop
+                for (int j = 0; j < endIndex; j++)
+                {
+                    if (elements[j].CompareTo(elements[j + 1]) > 0)
                     {
-                        if (elements[j].CompareTo(elements[j + 1]) > 0)
-                        {
-                            Swap(elements, j, j + 1);
-                            flag = true; // set flag as true to keep looping
-                        }
+                        Swap(elements, j, j + 1);
+                        swapped = true; // set flag as true to keep looping
                     }
-                    endIndex--;
                 }
+                endIndex--;
             }
         }
+
         public static void BubbleSort<T>(List<T> elements, IComparer<T> comparer)
         {
             int endIndex = elements.Count - 1;
-            bool flag = true;
+            bool swapped = true;
 
             for (int i = 0; i < elements.Count - 1; i++) // do as much as n-1
             {
-                while (flag) // to avoid unnecessary loop
-                {
-                    flag = false; // set flag as false to escape loop
+                if (!swapped) // to avoid unnecessary loop
+                    break;
 
-                    for (int j = 0; j < endIndex; j++)
+                swapped = false; // set flag as false to escape loop
+                for (int j = 0; j < endIndex; j++)
+                {
+                    if (comparer.Compare(elements[j], elements[j + 1]) > 0)
                     {
-                        if (comparer.Compare(elements[j], elements[j + 1]) > 0)
-                        {
-                            Swap(elements, j, j + 1);
-                            flag = true;
-                        }
+                        Swap(elements, j, j + 1);
+                        swapped = true;
                     }
-                    endIndex--;
                 }
+                endIndex--;
             }
         }
         public static void BubbleSort<T>(List<T> elements, Comparison<T> comparison)
         {
             int endIndex = elements.Count - 1;
-            bool flag = true;
+            bool swapped = true;
 
             for (int i = 0; i < elements.Count - 1; i++) // do as much as n-1
             {
-                while (flag) // to avoid unnecessary loop
-                {
-                    flag = false; // set flag as false to escape loop
+                if (!swapped) // to avoid unnecessary loop
+                    break;
 
-                    for (int j = 0; j < endIndex; j++)
+                swapped = false; // set flag as false to escape loop
+                for (int j = 0; j < endIndex; j++)
+                {
+                    if (comparison(elements[j], elements[j + 1]) > 0)
                     {
-                        if (comparison(elements[j], elements[j + 1]) > 0)
-                        {
-                            Swap(elements, j, j + 1);
-                            flag = true;
-                        }
+                        Swap(elements, j, j + 1);
+                        swapped = true;
                     }
-                    endIndex--;
                 }
+                endIndex--;
             }
         }
 
