9e8a993932f877ed7574b62aa6ac5c886140336a
Jason Kim <kims07231992@gmail.com>
2018-08-17 10:11:16 -0700

    Add PG5_DP_Bridge project

---
+++ C#/4_Design_Pattern/PG5_DP_Bridge/Lab72/PG5_DP_Bridge/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PG5_DP_Bridge
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+
+        }
+
+        private static void Run()
+        {
+
+        }
+    }
+}
