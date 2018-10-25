3b34538ce9518a2b4bdba3e9aa7640e6ad4c343f
Jason Kim <kims07231992@gmail.com>
2018-10-25 14:49:30 -0700

    Add a project for Dictionaries and Hashmaps > Sherlock and Anagrams

---
+++ C#/5_HackerRank/Algorithms/Dictionaries and Hashmaps/Sherlock and Anagrams/Sherlock and Anagrams/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sherlock_and_Anagrams
+{
+    internal class Program
+    {
+        private static int sherlockAndAnagrams(string s)
+        {
+
+        }
+
+        private static void Main(string[] args)
+        {
+            //TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+            int q = Convert.ToInt32(Console.ReadLine());
+
+            for (int qItr = 0; qItr < q; qItr++)
+            {
+                string s = Console.ReadLine();
+
+                int result = sherlockAndAnagrams(s);
+                Console.WriteLine(result);
+                //textWriter.WriteLine(result);
+            }
+
+            //textWriter.Flush();
+            //textWriter.Close();
+        }
+    }
+}
