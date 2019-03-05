28063e0a84aa8805d967c2393fe861c32a39c480
Jason Kim <kims07231992@gmail.com>
2019-03-04 23:19:18 -0800

    Add an extension class to encapsulate service registration processes

---
+++ C#/1_Web_Application/ASP.NET_Core_Angular/DotNetSurfer/DotNetSurfer.Web/Helpers/ServiceConfigurator.cs
@@ -0,0 +1,144 @@
+﻿using DotNetSurfer.Core.Encryptors;
+using DotNetSurfer.Core.TokenGenerators;
+using DotNetSurfer.DAL.Entities;
+using DotNetSurfer.DAL.Repositories;
+using DotNetSurfer.DAL.Repositories.Interfaces;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SpaServices.AngularCli;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
+using NLog;
+using NLog.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNetSurfer.Web.Helpers
+{
+    public static class ServiceConfigurator
+    {
+        public static void AddCoreServices(this IServiceCollection services)
+        {
+            services.AddSingleton<IEncryptor, HashEncryptor>();
+            services.AddSingleton<ITokenGenerator, JwtGenerator>();
+        }
+
+        public static void AddAngularServices(this IServiceCollection services)
+        {
+            services.AddSpaStaticFiles(configuration =>
+            {
+                configuration.RootPath = "ClientApp/dist";
+            });
+        }
+
+        public static void AddDatabaseServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<DotNetSurferDbContext>((options) =>
+            {
+                options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"));
+            });
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
+        }
+
+        public static void AddAuthenticationServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options =>
+                {
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuer = true,
+                        ValidateAudience = true,
+                        ValidateLifetime = true,
+                        ValidateIssuerSigningKey = true,
+                        ValidIssuer = configuration["Jwt:Issuer"],
+                        ValidAudience = configuration["Jwt:Issuer"],
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["Jwt:Key"]))
+                    };
+                });
+
+            services.AddAuthorization(options =>
+            {
+                options.AddPolicy(nameof(Permission), policy => policy.RequireRole(PermissionType.Admin.ToString()));
+            });
+        }
+
+        public static void AddAspDotNetCoreServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            // To avoid loop between primary and foreign keys
+            services.AddMvc().AddJsonOptions(options =>
+            {
+                options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
+            });
+
+            // Session
+            // Adds a default in-memory implementation of IDistributedCache.
+            services.AddDistributedMemoryCache();
+
+            int sessionExpireMinutes = Convert.ToInt32(configuration["Session:ExpireMinutes"]);
+            services.AddSession(options =>
+            {
+                options.IdleTimeout = TimeSpan.FromMinutes(sessionExpireMinutes);
+                options.Cookie.HttpOnly = true;
+            });
+
+            // ASP.NET Core
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+        }
+
+        public static void ConfigureAspDotNetCore(this IApplicationBuilder app)
+        {
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseAuthentication();
+            app.UseSession();
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller}/{action=Index}/{id?}");
+            });
+        }
+
+        public static void ConfigureAngular(this IApplicationBuilder app, IHostingEnvironment env)
+        {
+            app.UseSpaStaticFiles();
+            app.UseSpa(spa =>
+            {
+                spa.Options.SourcePath = "ClientApp";
+
+                if (env.IsDevelopment())
+                {
+                    spa.UseAngularCliServer(npmScript: "start");
+                }
+            });
+        }
+
+        public static void ConfigureExceptionHandler(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+                app.UseHsts();
+            }
+        }
+
+        public static void ConfigureLogHandler(ILoggerFactory loggerFactory, IConfiguration configuration)
+        {
+            GlobalDiagnosticsContext.Set("connectionString", configuration.GetConnectionString("DefaultConnection"));
+            loggerFactory.AddNLog();
+        }
+    }
+}
--- C#/1_Web_Application/ASP.NET_Core_Angular/DotNetSurfer/DotNetSurfer.Web/Startup.cs
+++ C#/1_Web_Application/ASP.NET_Core_Angular/DotNetSurfer/DotNetSurfer.Web/Startup.cs
@@ -1,20 +1,9 @@
-using System;
-using System.Text;
-using DotNetSurfer.DAL.Entities;
-using DotNetSurfer.DAL.Repositories;
-using DotNetSurfer.DAL.Repositories.Interfaces;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
+using DotNetSurfer.Web.Helpers;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.SpaServices.AngularCli;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
-using Microsoft.IdentityModel.Tokens;
-using NLog;
-using NLog.Extensions.Logging;
 
 namespace DotNetSurfer.Web
 {
@@ -30,98 +19,20 @@ namespace DotNetSurfer.Web
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            // EF Core DI
-            services.AddDbContext<DotNetSurferDbContext>((options) =>
-            {
-                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"));
-            });
-            services.AddScoped<IUnitOfWork, UnitOfWork>();
-
-            // To avoid loop between primary and foreign keys
-            services.AddMvc().AddJsonOptions(options =>
-            {
-                options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
-            });
-
-            // JWT
-            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-              .AddJwtBearer(options =>
-              {
-                  options.TokenValidationParameters = new TokenValidationParameters
-                  {
-                      ValidateIssuer = true,
-                      ValidateAudience = true,
-                      ValidateLifetime = true,
-                      ValidateIssuerSigningKey = true,
-                      ValidIssuer = Configuration["Jwt:Issuer"],
-                      ValidAudience = Configuration["Jwt:Issuer"],
-                      IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["Jwt:Key"]))
-                  };
-              });
-            services.AddAuthorization(options =>
-            {
-                options.AddPolicy(nameof(Permission), policy => policy.RequireRole(PermissionType.Admin.ToString()));
-            });
-
-            // Session
-            // Adds a default in-memory implementation of IDistributedCache.
-            services.AddDistributedMemoryCache();
-
-            int sessionExpireMinutes = Convert.ToInt32(Configuration["Session:ExpireMinutes"]);
-            services.AddSession(options =>
-            {
-                options.IdleTimeout = TimeSpan.FromMinutes(sessionExpireMinutes);
-                options.Cookie.HttpOnly = true;
-            });
-
-            // ASP.NET Core
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
-
-            // SPA
-            services.AddSpaStaticFiles(configuration =>
-            {
-                configuration.RootPath = "ClientApp/dist";
-            });
+            services.AddCoreServices();
+            services.AddAngularServices();
+            services.AddDatabaseServices(this.Configuration);
+            services.AddAuthenticationServices(this.Configuration);
+            services.AddAspDotNetCoreServices(this.Configuration);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                app.UseExceptionHandler("/Error");
-                app.UseHsts();
-            }
-
-            // NLog
-            GlobalDiagnosticsContext.Set("connectionString", Configuration.GetConnectionString("DefaultConnection"));
-            loggerFactory.AddNLog();
-
-            app.UseHttpsRedirection();
-            app.UseStaticFiles();
-            app.UseSpaStaticFiles();
-            app.UseAuthentication();
-            app.UseSession();
-            app.UseMvc(routes =>
-            {
-                routes.MapRoute(
-                    name: "default",
-                    template: "{controller}/{action=Index}/{id?}");
-            });
-
-            app.UseSpa(spa =>
-            {
-                spa.Options.SourcePath = "ClientApp";
-
-                if (env.IsDevelopment())
-                {
-                    spa.UseAngularCliServer(npmScript: "start");
-                }
-            });
+            ServiceConfigurator.ConfigureExceptionHandler(app, env);
+            ServiceConfigurator.ConfigureLogHandler(loggerFactory, this.Configuration);
+            app.ConfigureAspDotNetCore();
+            app.ConfigureAngular(env);
         }
     }
 }
