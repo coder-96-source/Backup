b913d43a9159492cf5fba13511fa976eb7f84e79
Jason Kim <kims07231992@gmail.com>
2019-12-21 22:26:42 -0800

    Migrated to other repos and cleaned README file

--- C#/1_Web_Application/ASP.NET_Core_Angular/DotNetSurfer/DotNetSurfer.Core/Encryptors/IEncryptor.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace DotNetSurfer.Core.Encryptors
-{
-    public interface IEncryptor
-    {
-        bool IsEqual(string value, string encryptedValue);
-        string Encrypt(string value);
-    }
-}
--- C#/1_Web_Application/ASP.NET_Core_Angular/DotNetSurfer/DotNetSurfer.Core/TokenGenerators/ITokenGenerator.cs
+++
@@ -1,9 +0,0 @@
-﻿using DotNetSurfer.DAL.Entities;
-
-namespace DotNetSurfer.Core.TokenGenerators
-{
-    public interface ITokenGenerator
-    {
-        string GetToken(User user);
-    }
-}
